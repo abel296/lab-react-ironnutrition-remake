585649c564180a769cb6aab2e1b498e9762bbc38
Abel Andrés <abelandres.gtc@gmail.com>
2023-02-16 18:13:30 +0100

    ♻️ change quantity input

--- client/src/components/FoodBox.js
+++ client/src/components/FoodBox.js
@@ -4,7 +4,8 @@ import getGifsService from '../services/getGifs.service';
 
 export default function FoodBox({ name, calories, image, showFoodGif, addPickedFood }) {
     const [gifState, setGifState] = useState('')
-    
+    const [quantityState, setQuantityState] = useState(1)
+
     useEffect(() => {
         async function getGif() {
             try {
@@ -16,32 +17,35 @@ export default function FoodBox({ name, calories, image, showFoodGif, addPickedF
         getGif()
     }, [name])
 
+    function handleInputChange(e) {
+        setQuantityState(e.currentTarget.value)
+    }
+
     return (
         <div className="box">
             <article className="media">
                 <div className="media-left">
                     <figure className="image is-64x64">
-                        <img src={image} alt=""/>
+                        <img src={ image } alt="" />
                     </figure>
                 </div>
                 <div className="media-content">
                     <div className="content">
                         <p>
-                            <strong>{name}</strong> <br />
-                            <small>{calories} cal</small>
+                            <strong>{ name }</strong> <br />
+                            <small>{ calories } cal</small>
                         </p>
                     </div>
                 </div>
                 <div className="media-right">
                     <div className="field has-addons">
                         <div className="control">
-                            <input className="input" type="number" defaultValue="1" />
+                            <input className="input" type="number" value={ quantityState } onChange={ e => handleInputChange(e) } />
                         </div>
                         <div className="control">
                             <button className="button is-info" onClick={ e => {
                                 showFoodGif(gifState, name)
-                                const quantity = e.currentTarget.parentElement.parentElement.querySelector('input').value
-                                addPickedFood(name, calories, quantity)
+                                addPickedFood(name, calories, quantityState)
                             } }>
                                 +
                             </button>
