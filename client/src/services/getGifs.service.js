4d5b9490e9e938dd72ec969a2a4bebadf0a17e94
Abel Andrés <abelandres.gtc@gmail.com>
2023-03-17 14:22:31 +0100

    🚀 set base url variable

--- client/src/services/getGifs.service.js
+++ client/src/services/getGifs.service.js
@@ -1,6 +1,6 @@
 export default async function getGifsService(name) {
     try {
-        const response = await fetch(`http://localhost:5000/gif/getSingleGif/${name}`)
+        const response = await fetch(`${process.env.REACT_APP_BASE_URL}/gif/getSingleGif/${name}`)
         return response.json()
     } catch (error) {
         console.error(error)
